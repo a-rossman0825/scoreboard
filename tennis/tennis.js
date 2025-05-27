f38c17c36a1fceaf2a0869ff579eaaa5bef82b02
Anthony Rossman <anthony.rossman0825@gmail.com>
2025-05-27 17:03:19 -0600

    start tennis script, Need refactor, need deuce logic

--- tennis/tennis.js
+++ tennis/tennis.js
@@ -6,9 +6,39 @@ let advantage = '';
 
 
 //WORK IN PROGRESS
-
-function increaseHomeScore() {
-  if (home = 40) {
-    
+/*
+function increaseScore(team) {
+  if (team < 30) {
+    team += 15;
+    drawScores();
+  } else if (team == 30) {
+    team += 10;
+    drawScores();
+  } 
+  if (home == 40 && away < 40) {
+    advantage = 'home';
+  } else if (home < 40 && away == 40){
+    advantage = 'away';
+  } else if (home == 40 && away == 40){
+    advantage = 'deuce';
+  } else {
+    advantage = '';
   }
+};
+
+function decreaseScore(team) {
+  if (team >= 40) {
+    team -= 10;
+  } else if (team <= 30 && team > 0){
+    team -= 15;
+  }
+  if (team == home && advantage == 'home')
 }
+
+  function drawScores() {
+    let homeScore = document.getElementById('home-score');
+    let awayScore = document.getElementById('away-score');
+    homeScore.innerText = home;
+    awayScore.innerText = away;
+  }
+  */
